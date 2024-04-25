eb50dcf435fadae63b45c38cf6eb233f455d920b
kindo <kindojnr58@gmail.com>
2024-04-25 11:23:42 +0000

    Services done

--- script.js
+++ script.js
@@ -50,3 +50,19 @@ Array.from(nextWord.children).forEach((letter, i) => {
 // Call changeText initially and set it to repeat every 300ms
 changeText();
 setInterval(changeText, 3000);
+
+
+//  Skills Glow
+const circles = document.querySelectorAll('.circle.')
+circles.forEach(elem=>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 / dots;
+
+    for(let i = 0; i < dots ; i++){
+        points += '<div class="points" style="--i:${i}; --rot:${rotatate}deg"></div>';
+    }
+    elem.innerHTML = points;
+})
