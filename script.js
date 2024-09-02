4933e4c3e210195aba823e07dea449d4b13bbd58
kindo <kindojnr58@gmail.com>
2024-09-02 23:57:25 +0000

    navbar readjusted

--- script.js
+++ script.js
@@ -112,7 +112,61 @@ circles.forEach(elem => {
 // Mix it up Section----------
 var mixer = mixitup('.portfolio-gallery')
 
-// swipper Section----------
+// Active menu----------
+let menuLi = document.querySelectorAll('header ul li a');
+let section = document.querySelectorAll('section');
 
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuLi.forEach(sec => sec.classList.remove("active"));
+    menuLi[len].classList.add("active");
+    }
+
+    activeMenu();
+    window.addEventListener("scroll",activeMenu),
+
+    // Sticky Navbar--------
+    document.addEventListener("DOMContentLoaded", function() {
+        const header = document.querySelector('header');
+        window.addEventListener("scroll", function() {
+            header.classList.toggle("sticky", window.scrollY > 50);
+        });
+    });
+
+   // Toggle Icon Navbar------
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x"); // Toggle the icon class
+    navlist.classList.toggle("open"); // Toggle the 'open' class on navlist
+}
+
+
+// Close the menu when a nav link is clicked
+navlist.querySelectorAll("a").forEach(link => {
+    link.onclick = () => {
+        menuIcon.classList.remove("bx-x");
+        navlist.classList.remove("open");
+    };
+});
+
+
+
+
+
+
+
+
+//   Tooltip-------------
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    });
+  });
+  
 
 
